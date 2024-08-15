f784f72ada3cd143b9291d62a7892a02e2a46b42
Antonio <antoniocarmagnani@hotmail.com>
2024-08-15 01:03:30 -0300

    add analytics file to test

--- assets/scripts/analytics.js
+++ assets/scripts/analytics.js
@@ -3,3 +3,45 @@ function trackButtonClick(buttonName) {
         'button_name': buttonName
     });
 }
+
+(function(){
+    "use strict";
+    /* global ga */
+    
+    var demo = true;
+    
+    var trackingLinks = document.getElementsByClassName('js-analytics-track'), i;
+  
+    function trackOnClick(event) {
+      var config = {
+            category: this.dataset.cat || 'PDF',
+            action: this.dataset.action || 'Download',
+            file: this.getAttribute('href')
+          };
+      
+      // Just delete this block in production environment
+      if(demo === true) {
+        event.preventDefault();
+        
+        var result = document.getElementById('m-result');
+        
+        result.innerHTML = [
+          'Google Analytics will track an event in the category',
+          config.category,
+          'action',
+          config.action,
+          'for the file',
+          config.file,
+          '.'
+        ].join(' ');
+      }
+      
+      if(typeof ga !== 'undefined') {
+        ga('send', 'event', category, action, file);
+      }
+    }
+  
+    for(i = 0; i < trackingLinks.length; i++) {
+      trackingLinks[i].addEventListener('click', trackOnClick);
+    }
+})();
