3ed02b2f2ac62ada85995215bd60f0a5099e4c3f
Antonio <antoniocarmagnani@hotmail.com>
2024-08-15 13:14:07 -0300

    adjust

--- assets/scripts/analytics.js
+++ assets/scripts/analytics.js
@@ -1,9 +1,9 @@
-function button_click(eventCategory, eventLabel, buttonName, buttonLocation, redirectUrl) {
+function button_click(eventCategory, eventLabel, buttonName, buttonLocation, destinationLabel) {
     gtag('event', 'Button Click', {
         'event_category': eventCategory,
         'event_label': eventLabel,
         'button_name': buttonName,
         'button_location': buttonLocation,
-        'destination': destinationLabel
+        'destination': destinationLabel,
     });
 }
