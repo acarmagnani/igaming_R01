1393f95a394b56341dba7688dffd912b14734973
Antonio <antoniocarmagnani@hotmail.com>
2024-08-15 11:17:12 -0300

    test

--- assets/scripts/analytics.js
+++ assets/scripts/analytics.js
@@ -1,4 +1,5 @@
 function trackWhatsAppClick(eventLabel) {
+    fbq('track', 'Lead');
     gtag('event', 'Button Click', {
       'event_category': 'WhatsApp Redirect',
       'event_label': eventLabel
