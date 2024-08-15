20173b8d2db6bac953288e3a939f4686d07bd524
Antonio <antoniocarmagnani@hotmail.com>
2024-08-15 12:58:17 -0300

    add google analytics event button

--- assets/scripts/analytics.js
+++ assets/scripts/analytics.js
@@ -1,6 +1,9 @@
-function trackWhatsAppClick(eventLabel) {
-    gtag('event', 'Whatsapp Click', {
-      'event_category': 'WhatsApp Redirect',
-      'event_label': eventLabel
+function button_click(event_label, event_label, button_name, button_location, redirect_url) {
+    gtag('event', 'Button Click', {
+        'event_category': event_category,
+        'event_label': event_label,
+        'button_name': button_name,
+        'button_location': button_location,
+        'redirect_url': redirect_url
     });
 }
